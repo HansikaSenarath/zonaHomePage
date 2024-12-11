73675bc0fd7dbc22aa40652e202df2db99148de7
HansiSenarath <your.email@example.com>
2024-12-11 19:40:04 +0530

    completed
    Completed all the sections and completed mobile responsive

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+function toggleNavbar() {
+  const navbarMobile = document.getElementById('navbar-mobile');
+  navbarMobile.classList.toggle('active');
+}
 
 
 const numberItems = document.querySelectorAll('.tech-land-number-item');
 let activeIndex = 0;
 
-// Filter only the indices for numbers 1 and 2
-const activeIndices = [0, 1]; // Corresponds to numbers 1 and 2
+const activeIndices = [0, 1]; 
 
 function cycleActiveState() {
-  // Remove active state from all items
   numberItems.forEach(item => item.classList.remove('active'));
 
-  // Set active state for the current active index
+
   const currentActiveIndex = activeIndices[activeIndex];
   numberItems[currentActiveIndex].classList.add('active');
 
-  // Increment the active index for the next cycle, loop within [0, 1]
   activeIndex = (activeIndex + 1) % activeIndices.length;
 }
 
-setInterval(cycleActiveState, 2000); // Adjust interval as needed
+setInterval(cycleActiveState, 2000);
 
 
